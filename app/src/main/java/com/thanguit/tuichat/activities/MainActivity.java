7b5fc97fb0a384405b5a2b7e396693efc52aeaf4
Chu Nam Thắng <namthangmu123@gamil.com>
2021-08-16 01:08:28 +0700

    01:08 16/8/2021

---
+++ app/src/main/java/com/thanguit/tuichat/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.thanguit.tuichat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thanguit.tuichat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/thanguit/tuichat/activities/PhoneNumberActivity.java
@@ -0,0 +1,16 @@
+package com.thanguit.tuichat.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.thanguit.tuichat.R;
+
+public class PhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_number);
+    }
+}
